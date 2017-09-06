86175b394433115ff513717e79cb9e698ff35bb5
SamuelXing <sam1796896099@163.com>
2017-09-06 00:01:40 -0400

    Sep 05, 2017

--- cpp_solutions/Array/Find_Peak_Element.cpp
+++ cpp_solutions/Array/Find_Peak_Element.cpp
@@ -7,9 +7,11 @@
 int findPeakElement(vector<int>& nums)
 {
 	 int start = 0, end = nums.size() - 1, mid=0;
+	 
 	 while(start<=end){
         mid=(start+end)/2; 
-		if((nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])){
+        // important
+		if((mid==0||nums[mid]>nums[mid-1]) &&( nums[mid]>nums[mid+1]||mid==nums.size() - 1)){
 			return mid;
 		}
 		else if(mid > 0 && nums[mid]<nums[mid-1]){
