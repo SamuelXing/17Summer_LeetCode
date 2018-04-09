83508e219150c58d304cdac72f2e1c2f27b166c8
SamuelXing <sam1796896099@163.com>
2018-04-08 23:12:52 -0400

    Apr 8, 2018

--- python_solutions/Tree/Binary_Tree_Level_Order_Traversal_II.py
+++ python_solutions/Tree/Binary_Tree_Level_Order_Traversal_II.py
@@ -25,3 +25,30 @@ class Solution:
                     newQ.append(node.right)
             Q = newQ
         return list(reversed(self.ret))
+
+# solution 2
+class Solution:
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if root is None:
+            return []
+        level = self.height(root)
+        self.ret = [[] for i in range(level)]
+        self.DFS(root, level-1, self.ret)
+        return self.ret
+        
+    def DFS(self, node, level, ret):
+        ret[level].append(node.val)
+        if node.left:
+            self.DFS(node.left, level-1, ret)
+        if node.right:
+            self.DFS(node.right, level-1, ret)
+    
+    def height(self, root):
+        if root is None:
+            return 0
+        return max(self.height(root.left), self.height(root.right)) + 1
+
