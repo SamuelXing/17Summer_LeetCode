bc3ab87b37331cac0c78b909ed51470570150c98
SamuelXing <zixing@syr.edu>
2018-05-29 13:44:59 -0400

    report

--- report.py
+++ report.py
@@ -5,7 +5,7 @@ import os.path
 totalfiles = 0
 
 for dirpath, dirnames, filenames in os.walk("."):
-    for filename in [f for f in filenames if f.endswith(".cpp")]:
+    for filename in [f for f in filenames if f.endswith(".cpp") or f.endswith(".py")]:
         totalfiles = totalfiles + 1
         print(os.path.join(dirpath, filename))
 
